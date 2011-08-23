8e458a719a8d14940d0430258fb1dd8076dabe2a
Brian Cable <briancable@Brian-Cable-iMac.local>
2011-08-23 15:48:55 -0500

    Initial Commit

---
+++ Clay/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Clay
+//
+//  Created by Brian Cable on 8/23/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ Clay/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Clay
+//
+//  Created by Brian Cable on 8/23/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
