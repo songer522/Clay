c92df226d295ca1632005a095a3d8eb2c38fe1a6
Brian Cable <briancable@Brian-Cable-iMac.local>
2011-08-24 14:12:46 -0500

    started running prototyping, added background, speed text, and running based on the rhythm of thumb presses

--- Clay/HelloWorldLayer.h
+++ Clay/HelloWorldLayer.h
@@ -14,11 +14,31 @@
 @interface HelloWorldLayer : CCLayer
 {
     CCSprite *sprite;
+    
+    CCSprite *background;
+    CCSprite *leftFoot;
+    CCSprite *rightFoot;
+    
+    CCLabelTTF *showDistanceTravelled;
+    CCLabelTTF *showVelocity;
+    
+    
+    float delayBeforeVisibleFoot;
+    float distanceTravelled;
+    float velocity;
+    float acceleration;
+    bool stalled;
+    int currentFoot;
+    
+    
 }
 
 // returns a CCScene that contains the HelloWorldLayer as the only child
 +(CCScene *) scene;
 
--(void) moveSprite:(CGPoint)touchLocation;
+-(void) doRun:(CGPoint)touchLocation;
+-(void) showFoot;
+-(void) updateText;
+-(void) stallOut;
 
 @end
--- Clay/HelloWorldLayer.m
+++ Clay/HelloWorldLayer.m
@@ -34,24 +34,44 @@
 	// always call "super" init
 	// Apple recommends to re-assign "self" with the "super" return value
 	if( (self=[super init])) {
+
+        CGSize screenSize = [[CCDirector sharedDirector] winSize];
+
+        background = [CCSprite spriteWithFile:@"forest.jpg"];
+        background.position = ccp(screenSize.width/2,screenSize.height/2);
+        background.scale = 1.0f;
+        [self addChild:background];
 		
 		// create and initialize a Label
-		CCLabelTTF *label = [CCLabelTTF labelWithString:@"Hello World" fontName:@"Marker Felt" fontSize:64];
+        showDistanceTravelled = [CCLabelTTF labelWithString:@"Distance Travelled: 0.00mi" dimensions:CGSizeMake(300, 50) alignment:UITextAlignmentLeft fontName:@"Arial" fontSize:18];
         
-        sprite = [CCSprite spriteWithFile:@"red_circle.png"];
-        sprite.scale = 0.15f;
-        sprite.position = ccp(300,300);
-        [self addChild:sprite];
+        showVelocity = [CCLabelTTF labelWithString:@"Velocity: 0.0mph" dimensions:CGSizeMake(300, 50) alignment:UITextAlignmentLeft fontName:@"Arial" fontSize:18];
         
-
-		// ask director the the window size
-		CGSize size = [[CCDirector sharedDirector] winSize];
-	
-		// position the label on the center of the screen
-		label.position =  ccp( size.width /2 , size.height/2 );
-		
-		// add the label as a child to this Layer
-		[self addChild: label];
+        [showDistanceTravelled setColor:ccWHITE];
+        [showVelocity setColor:ccWHITE];
+        showDistanceTravelled.position = ccp(200,200);
+        showVelocity.position = ccp(310,30);
+        [self addChild:showDistanceTravelled];
+        [self addChild:showVelocity];
+        
+        
+        velocity = 0.0f;
+        distanceTravelled = 0.0f;
+        stalled = true;
+        delayBeforeVisibleFoot = 1.0f;
+        acceleration = 0.0f;
+        currentFoot = 0;
+        
+        
+        leftFoot = [CCSprite spriteWithFile:@"left_foot.png"];
+        leftFoot.position = ccp(45, 50);
+        [self addChild:leftFoot];
+        leftFoot.opacity = 0.0f;
+        
+        rightFoot = [CCSprite spriteWithFile:@"right_foot.png"];
+        rightFoot.position = ccp(435, 50);
+        [self addChild:rightFoot];        
+        rightFoot.opacity = 0.0f;
         
         self.isTouchEnabled = YES;
         [self scheduleUpdate];
@@ -64,8 +84,7 @@
     NSSet *allTouches = [event allTouches];
     for(UITouch *touch in allTouches) {
         CGPoint touchLocation = [self convertTouchToNodeSpace:touch];
-        [self moveSprite:touchLocation];
-        [sprite setOpacity:255];
+        [self doRun:touchLocation];
     }
 }
 
@@ -74,23 +93,112 @@
     NSSet *allTouches = [event allTouches];
     for(UITouch *touch in allTouches) {
         CGPoint touchLocation = [self convertTouchToNodeSpace:touch];
-        [self moveSprite:touchLocation];
-        [sprite setOpacity:255];
+        [self doRun:touchLocation];
     }
 }
 
--(void) moveSprite:(CGPoint)touchLocation
+-(void) doRun:(CGPoint)touchLocation
 {
     NSLog(@"X:%f Y:%f",touchLocation.x,touchLocation.y);
-    sprite.position = touchLocation;
+    if (touchLocation.x <= 240) {
+        //check if currently left foot and visible
+        if(delayBeforeVisibleFoot <= 0.0f) {
+            if (currentFoot == 0) {
+                currentFoot = 1;
+                delayBeforeVisibleFoot = 0.40f - (acceleration / 5.0f);
+                if(delayBeforeVisibleFoot <=0) delayBeforeVisibleFoot = 0;
+                leftFoot.opacity = 0;
+                stalled = false;
+            } else {
+                currentFoot = 1;
+                [self stallOut];
+            }
+        } else {
+            [self stallOut];
+        }
+    } else {
+        if(delayBeforeVisibleFoot <= 0.0f) {
+            if (currentFoot == 1) {
+                currentFoot = 0;
+                delayBeforeVisibleFoot = 0.40f - (acceleration / 5.0f);
+                if(delayBeforeVisibleFoot <=0) delayBeforeVisibleFoot = 0;
+                rightFoot.opacity = 0;
+                stalled = false;
+            } else {
+                currentFoot = 0;
+                [self stallOut];
+            }
+        } else {
+            [self stallOut];
+        }
+    }
+}
+
+-(void) stallOut
+{
+    stalled = true;
+    velocity = (velocity / 3.0f) * 2.0f;
+    delayBeforeVisibleFoot = 0.4f;
+    rightFoot.opacity = 0;
+    leftFoot.opacity = 0;
+    [self updateText];
+}
+
+-(void) showFoot
+{
+    if (currentFoot == 0) {
+        leftFoot.opacity = 255;
+    } else {
+        rightFoot.opacity = 255;
+    }
+}
+
+-(void) updateText
+{
+    [showVelocity setString:[NSString stringWithFormat:@"Velocity: %.2fmph",velocity]];
+    [showDistanceTravelled setString:[NSString stringWithFormat:@"Distance Travelled: %.3fmi",distanceTravelled]];
 }
 
 -(void)update:(ccTime)dt
 {
-    NSLog(@"Update: %f",dt);
-    GLubyte opacity = sprite.opacity;
-    opacity -= dt*48;
-    [sprite setOpacity:opacity];
+    //NSLog(@"Update: %f",dt);
+    
+    if (delayBeforeVisibleFoot >= 0.0f) {
+        delayBeforeVisibleFoot -= dt;
+    }
+    
+    if (delayBeforeVisibleFoot <=0.0f && leftFoot.opacity == 0 && rightFoot.opacity == 0) {
+        [self showFoot];
+    }
+
+    
+    if (!stalled) {
+        if(delayBeforeVisibleFoot >= 0.0f) {
+            acceleration += 0.003f * dt;
+            if (acceleration > 2.0f) {
+                acceleration = 2.0f;
+            }
+            
+            velocity += acceleration;
+            if (velocity > 6.0f) {
+                velocity = 6.0f;
+            }            
+        }
+        
+    } else {
+        acceleration -= 0.003f * dt;
+        if(acceleration <= 0.0f) {
+            acceleration = 0.0f;
+        }
+        velocity += acceleration;
+        if(velocity <=0.0f) {
+            velocity = 0.0f;
+        }
+    }
+    
+    distanceTravelled += 0.000015f * velocity;
+    [self updateText];
+    
 }
 
 // on "dealloc" you need to release all your retained objects
