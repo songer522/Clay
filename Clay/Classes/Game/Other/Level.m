568cad20ddac990a4325d9016e0263d09ee39f0a
Brian Cable <bcable@xecudev.com>
2011-11-23 09:55:23 -0600

    Added blowing animation, doesn't have effect yet, wind needs repositioning. Also shifted the regionmanager so we should have no more update problems with it.

--- Clay/Classes/Game/Other/Level.m
+++ Clay/Classes/Game/Other/Level.m
@@ -572,7 +572,7 @@
     [self setPositionAtX:_x Y:_y];
     
     CGPoint playerPos = [[[LayerManager sharedLayers] getPlayer] getPosition];
-    [_obstacleManager changeRegionsBasedOnX:(playerPos.x - 128)];
+    [_obstacleManager changeRegionsBasedOnX:(playerPos.x - 256)];
     //[_backgroundManager changeRegionsBasedOnX:(playerPos.x - 128)];
     
     NSMutableArray *obstacles = [_obstacleManager getActiveGameObjectList];
