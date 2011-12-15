24a31908b91381541c9111a464fd8ad8c91b09e4
Brian Cable <bcable@xecudev.com>
2011-12-14 22:11:50 -0600

    Added options screen graphics and placed the sprites in the right spot on the screen.

--- Clay/Classes/Scenes/MainMenuScene.m
+++ Clay/Classes/Scenes/MainMenuScene.m
@@ -28,6 +28,7 @@
 #import "CreditsScene.h"
 #import "AppDelegate.h"
 #import "FBPrompt.h"
+#import "OptionsScene.h"
 
 
 
@@ -329,49 +330,15 @@
             break;
         case MENU_SWITCHTO_LEADERBOARDS:
             //[[GCHelper sharedInstance] showLeaderboards];
-            
-                
-                    /*
-                     {
-            NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-            if ([defaults objectForKey:@"FBAccessTokenKey"] 
-                && [defaults objectForKey:@"FBExpirationDateKey"])
-            {
-                facebook.accessToken = [defaults objectForKey:@"FBAccessTokenKey"];
-                facebook.expirationDate = [defaults objectForKey:@"FBExpirationDateKey"];
-            }*/
-            /*
-
-            if (![facebook isSessionValid]) 
-            {
-                [facebook authorize:nil];
-            }
-            */
-             /*       
-            NSMutableDictionary* params = [NSMutableDictionary dictionaryWithObjectsAndKeys:
-                                           @"264174546971482", @"app_id",
-                                           @"http://developers.facebook.com/docs/reference/dialogs/", @"link",
-                                           @"http://fbrell.com/f8.jpg", @"picture",
-                                           @"Facebook Dialogs", @"name",
-                                           @"hahahahahahah", @"caption",
-                                           @"yesyesyesyesyesyes.", @"description",
-                                           @"Facebook Dialogs are so easy!",  @"message",
-                                           nil];
-            
-            [facebook dialog:@"feed" andParams:params andDelegate:self];
-              }
-            */
             prompt = [FBPrompt promptWithAppId:@"264174546971482" andDelegate:self];
             [prompt showFacebookDialogWithDescription:@"Hello!" andPicture:@"http://fbrell.com/f8.jpg"];
-          
-                       break;
+           break;
         case MENU_SWITCHTO_ACHIEVEMENTS:
             [[GCHelper sharedInstance] showAchievements];
-        {
-        }
             break;
         case MENU_SWITCHTO_OPTIONS:
-            [[CCDirector sharedDirector] replaceScene:[CCTransitionFade transitionWithDuration:1.0f scene:[CreditsScene node]]];
+            [[CCDirector sharedDirector] replaceScene:[CCTransitionFade transitionWithDuration:1.0f scene:[OptionsScene node]]];
+            //[[CCDirector sharedDirector] replaceScene:[CCTransitionFade transitionWithDuration:1.0f scene:[CreditsScene node]]];
             break;
         default:
             break;
