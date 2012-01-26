46d8e97e3345b6cbab3763cbbbfd692721a71d8c
Brian Cable <bcable@xecudev.com>
2012-01-26 13:32:41 -0600

    level 11: added bumper animation

---
+++ Clay/Classes/Game/PlayerActions/PlayerActionPunch.h
@@ -0,0 +1,26 @@
+//
+//  PlayerActionPunch.h
+//  Clay
+//
+//  Created by Brian Cable on 1/26/12.
+//  Copyright (c) 2012 Xecudev, LLC. All rights reserved.
+//
+
+#import "PlayerAction.h"
+
+@class Projectile;
+@class Level;
+
+@interface PlayerActionPunch : PlayerAction
+{
+    Projectile *_kick;
+    bool _madeFootProjectile;
+    
+    //weak references
+    Level *_level;
+}
+
+-(void)testKickCollisions;
+-(void)updateBoundingBox;
+
+@end
