a465f6688c1886d6f73e7de5ac4e75a8ca10b308
ysongxecudev <songer522@gmail.com>
2012-02-14 12:43:41 -0600

    changed starting level to 1

--- Clay/Classes/Scenes/ChooseModeScene.m
+++ Clay/Classes/Scenes/ChooseModeScene.m
@@ -391,7 +391,7 @@
                 [[GameSettings shared] setNotNewForKey:@"storyHardUnlocked"];
             }
             [[GameSettings shared] setGlobal:@"NO" ForKey:@"titleMusicStarted"];
-            [[GameSettings shared] setGlobal:@"level10" ForKey:@"startingLevel"];
+            [[GameSettings shared] setGlobal:@"level1" ForKey:@"startingLevel"];
             [[GameSettings shared] setSerializedGlobal:@"level1" ForKey:@"storyModeCurrentLevel"];
             [[GameSettings shared] setSerializedGlobal:@"0" ForKey:@"storyModeCurrentTime"];
 
